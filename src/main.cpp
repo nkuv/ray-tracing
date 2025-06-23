01738cdc9868be2a5e41737575b1e55fd56cc4e2
Nandhu <nandhukrishna1217@gmail.com>
2025-06-23 22:49:57 +0530

    Added mouse movement, light source

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include<SFML/Graphics.hpp>
+#include<cmath>
 
 int main(){
-    sf::RenderWindow window(sf::VideoMode(1024,768),"Ray tracing");
-    window.setFramerateLimit(0);
+    sf::RenderWindow window(sf::VideoMode(1280,720),"Ray tracing");
+    window.setFramerateLimit(120);
 
     float posX = 0.f;
     float posY = 0.f;
-    float moveSpeed = 0.5f;
+    float moveSpeed = 5.f;
+    float lightPosX = 400.f;
+    float lightPosY = 300.f;
+    float smoothness = 0.1f;
 
     while(window.isOpen()){
         sf::Event event;
@@ -15,20 +19,33 @@ int main(){
                 window.close();
             } 
         }
+        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+
+        posX += (mousePos.x - posX - 100.f) * smoothness;
+        posY += (mousePos.y - posY - 100.f) * smoothness;
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))  posX -= moveSpeed;
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))  posX += moveSpeed;
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))  posY -= moveSpeed;
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))  posY += moveSpeed;
         
-        window.clear(sf::Color::Green);
+        window.clear(sf::Color::Black);
+
+        float dx = lightPosX - posX;
+        float dy = lightPosY - posY;
+        float distance = std::sqrt(dx * dx + dy * dy);
+        uint8_t brightness = static_cast<uint8_t>(255.f - (650.f-distance)/650.f);
+        brightness = std::max(std::min(brightness, (uint8_t)255), (uint8_t)20);
 
         sf::CircleShape sphere(100.f);
-        sphere.setFillColor(sf::Color::Red);
-        sphere.setPosition(
-            posX,
-            posY
-        );
+        sphere.setFillColor(sf::Color(brightness,0,0));
+        sphere.setPosition(posX,posY);
         window.draw(sphere);
+
+        sf::CircleShape light(20.f);
+        light.setFillColor(sf::Color::Yellow);
+        light.setPosition(lightPosX,lightPosY);
+        window.draw(light);
+
         window.display();
     }
 
